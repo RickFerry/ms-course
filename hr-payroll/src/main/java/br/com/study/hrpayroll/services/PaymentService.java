68542fb6c9e8f7e166f3bcff0e59dd8a974c5320
RickFerry <rick_ferry1@hotmail.com>
2023-02-05 14:43:56 -0300

    RestTempalte with lombok

--- hr-payroll/src/main/java/br/com/study/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/study/hrpayroll/services/PaymentService.java
@@ -3,22 +3,22 @@ package br.com.study.hrpayroll.services;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import br.com.study.hrpayroll.entities.Payment;
 import br.com.study.hrpayroll.entities.Worker;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class PaymentService {
 
     @Value("${hr-worker.host}")
     private String workerHost;
 
-    @Autowired
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
     public Payment getPayment(Long workerId, Integer days) {
         Map<String, String> uriVariables = new HashMap<>();
