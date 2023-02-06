3e36fe8bcae050fa129154beb9cc98b9473af3f8
RickFerry <rick_ferry1@hotmail.com>
2023-02-06 12:37:01 -0300

    Project hw-eureka-server created

---
+++ hr-eureka-server/src/test/java/br/com/example/study/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
